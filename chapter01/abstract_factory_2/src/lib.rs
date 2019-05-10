776bf1bf69ad4eebfd1dff7c34016256c0580e21
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2019-05-11 01:15:16 +0900

    Work in Progress

--- chapter01/abstract_factory_2/src/lib.rs
+++ chapter01/abstract_factory_2/src/lib.rs
@@ -1,21 +1,18 @@
 trait Hero {
-    fn new(name: String) -> Self;
-    fn interact_with<T: Obstacle>(&self, obstacle: &T);
+    fn new(name: String) -> Heros;
+    fn interact_with(&self, obstacle: Obstacles);
 }
 
 trait Obstacle {
-    fn new() -> Self;
+    fn new() -> Obstacles;
     fn name(&self) -> &String;
     fn action(&self) -> &String;
 }
 
 trait World {
-    type Hero;
-    type TestTest;
-
-    fn new(name: String) -> Self;
-    fn make_character(&self) -> Hero;
-    fn make_obstacle(&self) -> TestTest;
+    fn new(name: String) -> Worlds;
+    fn make_character(&self) -> Heros;
+    fn make_obstacle(&self) -> Obstacles;
 }
 
 pub enum WorldSelection {
@@ -23,9 +20,19 @@ pub enum WorldSelection {
     WizardWorld,
 }
 
-pub enum TheWorld {
+pub enum Worlds {
     FrogWorld(FrogWorld),
     WizardWorld(WizardWorld),
+}
+
+pub enum Heros {
+    Frog(Frog),
+    Wizard(Wizard),
+}
+
+pub enum Obstacles {
+    Bug(Bug),
+    Ork(Ork),
 }
 
 // Frog game
@@ -45,13 +52,13 @@ pub struct FrogWorld {
 }
 
 impl Hero for Frog {
-    fn new(name: String) -> Frog {
-        Frog {
+    fn new(name: String) -> Heros {
+        Heros::Frog(Frog {
             name: name,
-        }
+        })
     }
 
-    fn interact_with<T: Obstacle>(&self, obstacle: &T) {
+    fn interact_with(&self, obstacle: Obstacles) {
         let obs = obstacle.name();
         let act = obstacle.action();
 
@@ -63,11 +70,11 @@ impl Hero for Frog {
 }
 
 impl Obstacle for Bug {
-    fn new() -> Bug {
-        Bug {
+    fn new() -> Obstacles {
+        Obstacles::Bug(Bug {
             name: String::from("a bug"),
             action: String::from("eats it"),
-        }
+        })
     }
 
     fn name(&self) -> &String {
@@ -80,25 +87,22 @@ impl Obstacle for Bug {
 }
 
 impl World for FrogWorld {
-    type Hero = Frog;
-    type TestTest = Bug;
-
-    fn new(name: String) -> FrogWorld {
+    fn new(name: String) -> Worlds {
         let world_name = String::from("Frog World");
         println!("\n\n\t------ {} ------", world_name);
 
-        FrogWorld {
+        Worlds::FrogWorld(FrogWorld {
             world_name: world_name,
             player_name: name,
-        }
+        })
     }
     
-    fn make_character(&self) -> Frog {
+    fn make_character(&self) -> Heros {
         let player_name = self.player_name.clone();
         Frog::new(player_name)
     }
 
-    fn make_obstacle(&self) -> Bug {
+    fn make_obstacle(&self) -> Obstacles {
         Bug::new()
     }
 }
@@ -121,13 +125,13 @@ pub struct WizardWorld {
 }
 
 impl Hero for Wizard {
-    fn new(name: String) -> Wizard {
-        Wizard {
+    fn new(name: String) -> Heros {
+        Heros::Wizard(Wizard {
             name: name,
-        }
+        })
     }
 
-    fn interact_with<T: Obstacle>(&self, obstacle: &T) {
+    fn interact_with(&self, obstacle: Obstacles) {
         let obs = obstacle.name();
         let act = obstacle.action();
 
@@ -139,11 +143,11 @@ impl Hero for Wizard {
 }
     
 impl Obstacle for Ork {
-    fn new() -> Ork {
-        Ork {
+    fn new() -> Obstacles {
+        Obstacles::Ork(Ork {
             name: String::from("an evil ork"),
             action: String::from("kills it"),
-        }
+        })
     }
     fn name(&self) -> &String {
         &self.name
@@ -155,38 +159,35 @@ impl Obstacle for Ork {
 }
 
 impl World for WizardWorld {
-    type Hero = Wizard;
-    type TestTest = Ork;
-
-    fn new(name: String) -> WizardWorld {
+    fn new(name: String) -> Worlds {
         let world_name = String::from("Wizard World");
         println!("\n\n\t------ {} ------", world_name);
 
-        WizardWorld {
+        Worlds::WizardWorld(WizardWorld {
             world_name: world_name,
             player_name: name,
-        }
+        })
     }
 
-    fn make_character(&self) -> Wizard {
+    fn make_character(&self) -> Heros {
         let player_name = self.player_name.clone();
         Wizard::new(player_name)
     }
 
-    fn make_obstacle(&self) -> Ork {
+    fn make_obstacle(&self) -> Obstacles {
         Ork::new()
     }
 }
 
 // Game environment
 
-pub struct GameEnvironment<U, V> {
-    hero: U,
-    obstacle: V,
+pub struct GameEnvironment {
+    hero: String,
+    obstacle: String,
 }
 
-impl<U, V> GameEnvironment<U, V> {
-    pub fn new<W: World>(factory: W) -> GameEnvironment<U, V> {
+impl GameEnvironment {
+    pub fn new(factory: Worlds) -> GameEnvironment {
         let character = factory.make_character();
         let obstacle = factory.make_obstacle();
 
@@ -203,10 +204,10 @@ impl<U, V> GameEnvironment<U, V> {
 
 
 // This function is created because structs having a trait cannot expose their methods to outside.
-pub fn generate_world(world_name: WorldSelection, player_name: String) -> TheWorld {
+pub fn generate_world(world_name: WorldSelection, player_name: String) -> Worlds {
     match world_name {
-        WorldSelection::FrogWorld => TheWorld::FrogWorld(FrogWorld::new(String::from(player_name))),
-        WorldSelection::WizardWorld => TheWorld::WizardWorld(WizardWorld::new(String::from(player_name))),
+        WorldSelection::FrogWorld => FrogWorld::new(String::from(player_name)),
+        WorldSelection::WizardWorld => WizardWorld::new(String::from(player_name)),
     }
 }
 
